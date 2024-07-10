c86835795b5ca729dfbd5cf40efdbb955a1a14fe
damaestro165 <damaestro165@gmail.com>
2024-07-10 16:22:25 +0100

    feat: mobile nav

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -5,27 +5,27 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <main  className='flex flex-col'>
-      <section className='flex flex-col-reverse lg:flex-row lg:items-center mt-5 lg:px-20 xl:px-24 justify-center gap-5 xl:gap-0  '>
-       <div className='flex justify-center gap-4  px-8 lg:p-0 flex-col lg:w-1/2'>
-         <div className='flex gap-5  flex-col'>
+    <main  className='flex flex-col lg:px-0'>
+      <section className=' w-full flex flex-col-reverse lg:flex-row lg:items-center mt-5 lg:px-20 xl:px-24 justify-center gap-5 xl:gap-0 '>
+       <div className='flex flex-col justify-center   px-4 w-full gap-4  lg:p-0  lg:w-1/2'>
+         <div className='flex gap-6 lg:gap-5 flex-col'>
           <h1 className='text-2xl lg:text-4xl lg:pr-8 xl:text-5xl xl:leading-[3.5rem] font-bold xl:pr-6'>
             Experience Immersive <span className=' text-green-700' > Language
                Learning</span>  through Live Meetings
           </h1>
           <p className='font-[400] text-sm'>Join Interactive Meetings led by Native Speakers Anytime, Anywhere</p>
         </div>
-        <div className='flex gap-5'>
-          <Button className='bg-green-700 w-44 '> Get Started</Button>
-          <Button variant='outline' className='border border-green-700 w-44 '> Log In</Button>
+        <div className='flex gap-6 lg:gap-5'>
+          <Button className='bg-green-700 w-36 lg:w-44 '> Get Started</Button>
+          <Button variant='outline' className='border border-green-700 w-36 lg:w-44 '> Log In</Button>
         </div>
          <div className='flex gap-16'>
          <div>
-          <h2 className='text-4xl'>20K+</h2>
+          <h2 className=' text-2xl lg:text-4xl'>20K+</h2>
           <p className='text-sm'>Active users</p>
          </div>
          <div>
-          <h2 className='text-4xl'>2+</h2>
+          <h2 className=' text-2xl lg:text-4xl'>2+</h2>
           <p className='text-mm'>Available in Nigeria and Ghana</p>
          </div>
           
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,15 +1,26 @@
 import Link from 'next/link'
 import React from 'react'
 import { Button } from "@/components/ui/button"
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import { HamburgerMenuIcon } from '@radix-ui/react-icons'
+
 
 
 const NavBar = () => {
   return (
-    <nav className='flex py-5 justify-center items-center w-full'>
-        <div className='flex justify-center w-1/5'>
+   <section className='w-full'>
+     <nav className=' hidden lg:flex py-5 justify-center items-center w-full'>
+        <div className='flex justify-center w-1/5 '>
             Langner
         </div>
-         <div className='flex justify-center items-center w-2/5 '>
+         <div className='flex  justify-center items-center w-2/5 '>
           <Button variant='link'><Link href='/'>Home</Link></Button>
           <Button variant='link'><Link href='/'>About Us</Link></Button> 
           <Button variant='link'><Link href='/'>How it works</Link></Button>  
@@ -20,6 +31,26 @@ const NavBar = () => {
             <Button variant='outline' className=' border border-green-700 w-36'>Log In</Button>
         </div>
     </nav>
+    <nav className='flex p-4 justify-between  lg:hidden '>
+      <div className='w-1/2 text-xl flex items-center font-bold' >
+            Langner
+      </div>
+     <div>
+       <Sheet>
+          <SheetTrigger> 
+            <Button variant="outline" size="icon">
+              < HamburgerMenuIcon className="h-4 w-4" />
+            </Button>
+    </SheetTrigger>
+          <SheetContent>
+           <div className='w-1/2 text-xl font-bold' >Langner</div>
+          </SheetContent>
+        </Sheet>
+     </div>
+
+
+    </nav>
+   </section>
   )
 }
 
