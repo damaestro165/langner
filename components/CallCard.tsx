f7edaef4b55e89dde5bbab1c87e47b98cef62252
damaestro165 <damaestro165@gmail.com>
2025-04-14 16:27:01 +0100

    chore

--- components/CallCard.tsx
+++ components/CallCard.tsx
@@ -41,13 +41,13 @@ const CallCard = ({
   };
 
   return (
-    <section className="flex min-h-52 w-full flex-col justify-between rounded-xl p-2 xl:max-w-1/2 bg-[#506674ff] text-white ">
+    <section className="flex w-full flex-col gap-2 rounded-xl p-2 xl:max-w-1/2 bg-[#506674ff] text-white ">
       <article className="flex flex-col gap-5">
         <Image src={icon} alt="meeting-image" width={28} height={28} />
         <div className="flex justify-between">
           <div className="flex flex-col gap-2 truncate">
             <h4 className="text-xl font-bold truncate">{title}</h4>
-            <p className="text-base font-normal">{meetingDate}</p>
+            <p className="text-sm font-normal">{meetingDate}</p>
           </div>
         </div>
       </article>
